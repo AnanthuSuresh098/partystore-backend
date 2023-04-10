e14f61c56d87e863515151d301d738d17c2151ce
AnanthuSuresh098 <93374262+AnanthuSuresh098@users.noreply.github.com>
2023-04-10 15:26:11 +0530

    Update db.js

--- src/configs/db.js
+++ src/configs/db.js
@@ -7,11 +7,7 @@ const MONGO_URL = process.env.MONGO_URL;
 
 module.exports = () => {
     mongoose.set("strictQuery", false);
-  return mongoose.connect(
-    "mongodb+srv://partystore01789:TXew9enmflo9Om7N@cluster0.zwtsctq.mongodb.net/?retryWrites=true&w=majority",
-    {
-      useUnifiedTopology: true,
-      useNewUrlParser: true,
-    }
-  );
+  return mongoose.connect(MONGO_URL,{
+    useUnifiedTopology:true,useNewUrlParser:true
+  });
 };
