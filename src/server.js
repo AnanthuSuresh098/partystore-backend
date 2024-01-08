00a3def5f396898a7cf37b17bd36c26c9de1743d
AnanthuSuresh098 <93374262+AnanthuSuresh098@users.noreply.github.com>
2024-01-09 01:51:37 +0530

    Update server.js

--- src/server.js
+++ src/server.js
@@ -6,10 +6,10 @@ const connect = require("./configs/db");
 
 // const PORT=process.env.PORT||4000;
 
-app.listen(8080, async () => {
+app.listen(5000, async () => {
   try {
     await connect();
-    console.log(`listening on port 8080`);
+    console.log(`listening on port 5000`);
   } catch (err) {
     console.log(err);
   }
